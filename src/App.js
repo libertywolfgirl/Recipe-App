16a83e15f3ee756023e4711247adb3f223179bd1
Glitch (starter-react-template) <none>
2021-04-03 01:23:49 +0000

    📅🎽 Checkpoint ./src/App.js:16231546/1378 ./src/Tweet.js:16231546/73

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
-import Tweet from './Tweet';
+import React, { useState } from "react";
+import Tweet from "./Tweet";
 
 function App() {
-  
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className='app'>
-      <Tweet name="Dev ED" message="This is a random tweet" likes="506"/>
-      <Tweet name="John Snow" message="I am the true king" likes=""/>
-      <Tweet name="Traversy Media" message="700k my dudes"/>
-      <Tweet name="Mosh" message="My new course is available"/>
+    <div className="app">
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
