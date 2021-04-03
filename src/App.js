dcfdb86f5939dde156f96be92db26e95c6089330
Glitch (starter-react-template) <none>
2021-04-03 21:49:18 +0000

    🐿👖 Checkpoint ./src/App.js:16231546/256

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ const App = () => {
   //const APP_ID = 'dceb7501';
   //const APP_KEY = '150a19f05e09caac72a633e8f428c806';
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   const getRecipes = async () => {
+    const proxyUrl = 'https://secret-ocean-49799.herokuapp.com/';
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+      `${proxyUrl}https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
     );
     const data = await response.json();
     console.log(data);
