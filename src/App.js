0d0e4b24dcfab060afb6eed714d140c533049097
Glitch (starter-react-template) <none>
2021-04-03 00:59:50 +0000

    💏🍯 Checkpoint ./src/Tweet.js:16231546/257 ./src/App.js:16231546/65 ./src/App.css:16231546/798 ./src/index.css:16231546/63

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Tweet from './Tweet';
 
 function App() {
   
   return (
-    <div>
+    <div className='app'>
       <h1>Hello React</h1>
+      <Tweet />
     </div>
   );
 }
