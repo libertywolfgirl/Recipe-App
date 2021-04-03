26c8bd7b3f1572b9290a352885720caed452457b
Glitch (starter-react-template) <none>
2021-04-03 20:12:43 +0000

    🌓🚇 Checkpoint ./src/App.js:16231546/2308

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ const App = () => {
   //const APP_ID = 'dceb7501';
   //const APP_KEY = '150a19f05e09caac72a633e8f428c806';
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+  useEffect( () => {
+    
+  }, []);
+  
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
 
-  const [counter, setCounter] = useState(0);
-  
-  useEffect(() => {
-    console.log('Effect has been run');
-  });
-  
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">
-          {}
+          Search
         </button>
       </form>
     </div>
