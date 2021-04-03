fb33de83d40ae1d422f7496131a1673f5202b6ce
Glitch (starter-react-template) <none>
2021-04-03 20:24:23 +0000

    🍀🌍 Checkpoint ./src/App.js:16231546/1468

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ const App = () => {
   //const APP_ID = 'dceb7501';
   //const APP_KEY = '150a19f05e09caac72a633e8f428c806';
 
-  useEffect( () => {
-    
-  }, []);
-  
+  useEffect(() => {}, []);
+
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+    );
     const data = await response.json();
     console.log(data);
-  }
+  };
 
   return (
     <div className="App">
