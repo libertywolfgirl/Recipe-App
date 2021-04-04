83db2827fc75503f77c71d443040e50fbe5ed80c
Glitch (starter-react-template) <none>
2021-04-04 00:22:36 +0000

    🚟👩‍👩‍👦 Checkpoint ./src/recipe.module.css:16231546/654 ./src/Recipe.js:16231546/1001

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,17 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
       <ol>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
-      <img src={image} alt="" />
+      <p>{Math.round(calories*100)/100} calories</p>
+      <img className={style.image} src={image} alt="" />
     </div>
   );
 };
